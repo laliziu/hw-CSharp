867c3f8909a47ee09873c3f675928bebe804d9e4
Lali <lalizyulkorn@gmail.com>
2023-02-01 15:18:54 +0400

    дз к 4 семинару

---
+++ sem4/task1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+
+
+int GetNumber2(string text)
+{
+    Console.Write(text);
+    int numberB = int.Parse(Console.ReadLine());
+    return numberB;
+}
+
+int GetNumber(string text)
+{
+    Console.Write(text);
+    int numberA = int.Parse(Console.ReadLine());
+    return numberA;
+}
+
+
+int PowerNum(int numberA, int numberB)
+{
+int result = numberA;
+for (int count = 2; count <= numberB; count++)
+    {
+        result = result * numberA;
+    }
+    return result;
+}
+
+
+int numberA = GetNumber("enter the number A: ");
+int numberB = GetNumber2(" enter the number B: ");
+int result = PowerNum (numberA, numberB);
+Console.WriteLine($"{numberA}, {numberB} -> {result}");
---
+++ sem4/task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int GetSum(int number)
+{
+    int result = 0;
+    int temp = number;
+    for (int i = 0; i < number.ToString().Length; i++)
+    {
+        result = result + temp % 10;
+        temp = temp / 10;
+    }
+    return result;
+}
+
+int number = GetNumber("enter the number: ");
+int result = GetSum(number);
+Console.WriteLine($"{number} -> {result}");
