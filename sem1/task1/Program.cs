67d3558862fb1029c00261d4bb25b883b37cdf91
Lali <lalizyulkorn@gmail.com>
2023-02-01 14:46:21 +0400

    дз к 1 семинару

---
+++ sem1/task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("enter 1st number: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("enter 2nd number: ");
+int num2 = int.Parse(Console.ReadLine());
+int max = 0;
+int min = 0;
+
+if (num1 > num2)
+{
+    max = num1;
+    min = num2;
+}
+else
+{
+    max = num2;
+    min = num1;
+}
+Console.WriteLine("max = " + max + "  min = " + min);
---
+++ sem1/task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("enter 1st number: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("enter 2nd number: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("enter 3rd number: ");
+int num3 = int.Parse(Console.ReadLine());
+int max = num1;
+
+if (num1 > max) max = num1;
+
+if (num2 > max) max = num2;
+
+if (num3 > max) max = num3;
+
+Console.Write("max = " + max);
