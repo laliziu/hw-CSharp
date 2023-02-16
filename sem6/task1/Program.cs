e69b059e202ae5be8a3d2cfd0be0df4e0e8ecce9
Lali <lalizyulkorn@gmail.com>
2023-02-17 01:41:10 +0400

    дз к 6 семинару

---
+++ sem6/task1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+int MoreThanZero(int[] array)
+{
+    int count = 0;
+    for (int index = 0; index < array.Length; index++)
+    {
+        if (array[index] > 0) count++;
+    }
+    return count;
+}
+
+
+int[] GetArray(int size, int from, int to)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(from, (to + 1));
+    }
+    return array;
+}
+
+int GetSize(string text)
+{
+    Console.WriteLine(text);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int size = GetSize("Amount of numbers: ");
+int[] array = GetArray(size, -100, 999);
+int count = MoreThanZero(array);
+Console.Write(String.Join(", ", array));
+Console.WriteLine($" -> {count}");
---
+++ sem6/task2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 43: Напишите программу,
+// которая найдёт точку пересечения двух прямых,
+// заданных уравнениями
+// y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// пусть б1 = 1, к1=2, б2=3, к2=4
+// K1x+B1=K2X+B2   2x+1=4x+3  y=2*-1+1=-1
+// K1x-K2X=B1+B2   2x-4x=3-1
+//                -2x=2 x=-1   
+
+string PointCrossed(int B1, int K1, int B2, int K2)
+{
+    double x = (B2 - B1) / (K1 - K2);
+    double y = (K1 * (B2 - B1)) / (K1 - K2) + B1;
+    Console.WriteLine($"{x},{y}");
+    return $"({x}; {y})";
+}
+
+
+int GetNum(string text)
+{
+    Console.Write(text);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int numberB1 = GetNum("Enter number B1: ");
+int numberK1 = GetNum("Enter number K1: ");
+int numberB2 = GetNum("Enter number B2: ");
+int numberK2 = GetNum("Enter number K2: ");
+Console.Write($"b1 = {numberB1}, k1 = {numberK1}, b2 = {numberB2}, k2 = {numberK2} -> ");
+PointCrossed(numberB1, numberK1, numberB2, numberK2);
